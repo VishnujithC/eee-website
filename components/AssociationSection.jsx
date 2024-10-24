4979e7da646040b76fb428fbb834ddf37ada7ffc
anima-regem <vichukartha@gmail.com>
2024-10-25 04:51:50 +0530

    stable

--- components/AssociationSection.jsx
+++ components/AssociationSection.jsx
@@ -1,28 +1,24 @@
 import styles from "@styles/acheivementsSection.module.scss";
 import FacultyCard from "./FacultyCard";
+import Image from "next/image";
 import getDetailsFromFirebase from "@util/getDetailsFromFirebase";
 import { cookies } from "next/headers";
 
 const AssociationSection = async () => {
-  const cookieStore = cookies()
-
+  const cookieStore = cookies();
   const faculties = await getDetailsFromFirebase("association");
-
-  console.log("hello")
 
   return (
     <div className={styles.main_wrapper}>
       <h3 className={styles.heading}>Core Team</h3>
       <div className={styles.wrapper}>
-        {faculties.map((faculty) => {
-          return (
-            <FacultyCard
-              name={faculty.facultyName}
-              image={faculty.image}
-              designation={faculty.facultyDesignation}
-            />
-          );
-        })}
+        <div className={styles.imageContainer}>
+          <img
+            src="./images/Association.jpg"
+            alt="faculty"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </div>
       </div>
     </div>
   );
